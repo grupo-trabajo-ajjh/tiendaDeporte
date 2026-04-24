97fce2c31bef83b7b5a06fe692e1f70dfadc3913
Nombre del usuario 2 <usuario2@ejemplo.com>
2026-04-24 12:59:11 +0200

    cambio de clase alta

--- src/main/java/es/tienda/modelos/Articulos.java
+++ src/main/java/es/tienda/modelos/Articulos.java
@@ -1,4 +1,37 @@
 package es.tienda.modelos;
 
 public class Articulos {
+
+    private String codigo;
+    private String descripcion;
+    private double precio;
+
+    public Articulos() {
+        this.codigo = "";
+        this.descripcion = "";
+        this.precio = 0.0;
+    }
+
+    public Articulos(String codigo, String descripcion, double precio) {
+        this.codigo = codigo;
+        this.descripcion = descripcion;
+        this.precio = precio;
+    }
+
+    public void visualizarDatos() {
+        System.out.println("\n--- Datos del Artículo ---");
+        System.out.println("Código:      " + this.codigo);
+        System.out.println("Descripción: " + this.descripcion);
+        System.out.println("Precio:      $" + this.precio);
+        System.out.println("--------------------------");
+    }
+
+    public Articulos alta(String codigo, String descripcion, double precio) {
+        Articulos nuevoArticulo = new Articulos(codigo, descripcion, precio);
+        System.out.println("Articulo dado de alta correctamente");
+        nuevoArticulo.visualizarDatos();
+        return nuevoArticulo;
+    }
+
+
 }
