aabda6d32cb38aea505031c6b43202d9dd133a02
Juan David Fernández García <juandavidfernandez988g@gmail.com>
2026-04-24 12:46:12 +0200

    He añadido el enum TipoProveedor a la clase Proveedor.java

--- src/main/java/es/tienda/modelos/Proveedor.java
+++ src/main/java/es/tienda/modelos/Proveedor.java
@@ -2,15 +2,31 @@ package es.tienda.modelos;
 
 
 public class Proveedor extends Persona {
-
-
-    public Proveedor(String nombre, String apellidos, String dni, String nombreEmpresa, String telefono) {
-        super(nombre, apellidos, dni);
-
+    public enum TipoProveedor {
+        SERVICIO,
+        RECURSOS,
+        FABRICANTE
     }
+
+    private TipoProveedor tipoProveedor;
+
+    public Proveedor(String nombre, String apellidos, String dni, TipoProveedor tipoProveedor) {
+        super(nombre, apellidos, dni);
+        this.tipoProveedor = tipoProveedor;
+    }
+
+    public TipoProveedor getTipoProveedor() {
+        return tipoProveedor;
+    }
+
+    public void setTipoProveedor(TipoProveedor tipoProveedor) {
+        this.tipoProveedor = tipoProveedor;
+    }
+
     @Override
     public void mostrarDatos() {
         System.out.println("Proveedor: " + getNombre() + " " + getApellidos());
         System.out.println("DNI: " + getDni());
+        System.out.println("Tipo: " + tipoProveedor);
     }
 }
