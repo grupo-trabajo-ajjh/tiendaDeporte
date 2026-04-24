6e842e6c99cabef72fcd56097a816bd6eac05699
usuario2 <usuario2@ejemplo.com>
2026-04-24 12:44:21 +0200

    He añadido un atributo más tipoCliente como un enumerado con los siguientes valores: POTENCIAL, NUEVO, VIP, MOROSO

--- src/main/java/es/tienda/modelos/Cliente.java
+++ src/main/java/es/tienda/modelos/Cliente.java
@@ -2,14 +2,30 @@ package es.tienda.modelos;
 
 public class Cliente extends Persona {
 
-    public Cliente(String nombre, String apellidos, String dni, String email, String direccion) {
-        super(nombre, apellidos, dni);
+    public enum TipoCliente {
+        POTENCIAL,
+        NUEVO,
+        VIP,
+        MOROSO
     }
+
+    private String email;
+    private String direccion;
+    private TipoCliente tipoCliente;
+
+    public Cliente(String nombre, String apellidos, String dni, String email, String direccion, TipoCliente tipoCliente) {
+        super(nombre, apellidos, dni);
+        this.tipoCliente = tipoCliente;
+    }
+
+    public TipoCliente getTipoCliente() { return tipoCliente; }
+    public void setTipoCliente(TipoCliente tipoCliente) { this.tipoCliente = tipoCliente; }
 
     @Override
     public void mostrarDatos() {
         System.out.println("Cliente: " + getNombre() + " " + getApellidos());
         System.out.println("DNI: " + getDni());
+        System.out.println("Tipo Cliente: " + tipoCliente);
     }
 }
     
